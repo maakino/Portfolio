edfc86fefe9df0633516529d08719cf4168c7b3b
Maakyno <teddygoss1@hotmail.com>
2022-04-27 12:45:58 +0400

    configuration menu mobile

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var nav = document.getElementById('nav');
+var burger = document.getElementById('burger');
+var body = document.querySelector('body');
+var links = document.querySelectorAll('.header__menu-links')
+
+
+
+function toggleNav(){
+    nav.classList.toggle('nav__open');
+    body.classList.toggle('noScroll');
+}
+
+
+burger.addEventListener('click',toggleNav);
+
+links.forEach(function(link){
+    link.addEventListener('click',toggleNav);
+})
+
+
+
