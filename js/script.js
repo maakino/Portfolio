c8570da3bb7232bd23099be41c9392e760e981f8
Maakyno <teddygoss1@hotmail.com>
2022-04-28 20:39:37 +0400

    Add font familly and css home mobile

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
+/* Menu mobile */
 var nav = document.getElementById('nav');
 var burger = document.getElementById('burger');
 var body = document.querySelector('body');
 var links = document.querySelectorAll('.header__menu-links')
-
-
 
 function toggleNav(){
     nav.classList.toggle('nav__open');
     body.classList.toggle('noScroll');
 }
 
-
 burger.addEventListener('click',toggleNav);
 
 links.forEach(function(link){
     link.addEventListener('click',toggleNav);
 })
+
 
 
 
