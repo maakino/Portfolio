77c340e19a0d0e44073a2e9c52eefe70b679566a
Maakyno <teddygoss1@hotmail.com>
2022-05-15 17:27:37 +0400

    add popin responsive portfolio

--- js/script.js
+++ js/script.js
@@ -1,35 +1,33 @@
-function isMobileDevice() { 
-    if( navigator.userAgent.match(/iPhone/i) ||
-     navigator.userAgent.match(/webOS/i) ||
-     navigator.userAgent.match(/Android/i) ||
-     navigator.userAgent.match(/iPad/i) ||
-     navigator.userAgent.match(/iPod/i) ||
-     navigator.userAgent.match(/BlackBerry/i) ||
-     navigator.userAgent.match(/Windows Phone/i)
-    ){
-       return true;
-     }
+function isMobileDevice() {
+    if (navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i) ||
+        navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        return true;
+    }
     else {
-       return false;
-     }
-   }
-
+        return false;
+    }
+}
 /* Menu mobile */
 var nav = document.getElementById('nav');
 var burger = document.getElementById('burger');
 var body = document.querySelector('body');
 var links = document.querySelectorAll('.header__menu-links');
-var cards = document.querySelectorAll('.cards');
+var cards = document.querySelectorAll('.js-card');
 var header = document.querySelector('#header');
-
-
+const dialogs = document.querySelectorAll('.js-popin');
+const btn = document.querySelector('.js-btn');
 
 function toggleNav() {
     nav.classList.toggle('nav__open');
-    body.classList.toggle('noScroll');
+    body.classList.toggle('nav-open');
     header.classList.toggle('nobckg');
 }
-
 burger.addEventListener('click', toggleNav);
 
 links.forEach(function (link) {
@@ -37,23 +35,38 @@ links.forEach(function (link) {
 })
 
 function showCard() {
-
-    let txts = this.querySelectorAll('.service__card-textOff');
-    let logoService = this.querySelectorAll('.logo-service');
-
-    txts.forEach(item => {
-        item.classList.toggle('card-textOn');
+    const title = this.getAttribute("alt");
+    let src = this.getAttribute("src");
+    src = 'img/portfolio/full-size/' + src.substring(src.lastIndexOf('/') + 1);
+    body.classList.add('noScroll');
+    dialogs.forEach(function (dialog) {
+        dialog.setAttribute("open", "true");
+        const titleElement = dialog.querySelector('.js-title');
+        titleElement.innerHTML= title;
+        const srcElement = dialog.querySelector('.js-img');
+        srcElement.setAttribute("src", src);
     })
-    logoService.forEach(item => {
-        item.classList.toggle('logo-off');
-    })
+
 }
-
 cards.forEach(function (card) {
-    if (isMobileDevice()){
-        card.addEventListener('click', showCard);
-    }
+    card.addEventListener('click', showCard)
 })
+
+
+function closePopin(){
+    dialogs.forEach(function (dialog){
+         dialog.removeAttribute("open"); 
+         body.classList.remove('noScroll');
+    })
+};
+btn.addEventListener('click', closePopin);
+
+
+
+
+
+
+
 
 
 
